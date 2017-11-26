aed266f37f5f6c4fec5b61032d0ddd8d2905f281
Denis <carriere.denis@gmail.com>
2017-11-25 20:00:02 -0800

    Remove stderr from 2000-node-highways

--- models/2000-node-highways/end.js
+++ models/2000-node-highways/end.js
@@ -1,14 +1,15 @@
 module.exports = () => {
   const results = global.osmlinter.results
-  const total = Object.keys(results).length
+  let total = 0
   process.stderr.write(`${total} GeoJSON Features scanned\n`)
 
   Object.keys(results).forEach(id => {
     const nodes = results[id]
     if (nodes > 1500) {
       const msg = `@id ${id} => ${nodes} Nodes\n`
-      process.stderr.write(msg)
       process.stdout.write(msg)
+      total++
     }
   })
+  process.stderr.write(`${total} GeoJSON Features with 1500+ nodes\n`)
 }
